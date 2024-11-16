0cff4f0084709794ffe4b2a9e7cb198c6d5500cd
Ekofi Research <erdoganferhatsuat@yahoo.com>
2024-11-16 15:41:38 +0300

    u

--- app/layout.js
+++ app/layout.js
@@ -4,6 +4,8 @@ import Script from "next/script";
 import GoogleAdsense from "../comp/GoogleAdsense";
 import FacebookPixel from "../comp/FacebookPixel";
 import GoogleAds from "../comp/GoogleAds";
+import Script from "next/script";
+import { GA_TRACKING_ID } from "@/lib/gtag";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -15,6 +17,24 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <Script
+          strategy="afterInteractive"
+          src={`https://www.googletagmanager.com/gtag/js?id=${GA_TRACKING_ID}`}
+        />
+        <Script
+          id="google-analytics"
+          strategy="afterInteractive"
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${GA_TRACKING_ID}');
+        `,
+          }}
+        />
+      </head>
       <body className="flex h-full items-center text-center justify-center text-bg-dark">
         {process.env.NODE_ENV === "production" && <GoogleAds />}
         {children}
