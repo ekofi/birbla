9dd7a4636290b89764aa469ccea794e3a391f2d1
Ekofi Research <erdoganferhatsuat@yahoo.com>
2025-04-09 16:51:23 +0300

    Update NewsletterSubscribe.jsx

--- comp/NewsletterSubscribe.jsx
+++ comp/NewsletterSubscribe.jsx
@@ -80,7 +80,6 @@ export default function NewsletterSubscribe() {
             name="_subject"
             value="New newsletter subscription!"
           />
-          <input type="hidden" name="_captcha" value="false" />
           <input type="hidden" name="_template" value="table" />
           <input
             type="hidden"
